f9a7d5db1c9ff3b50caab9be4e10a8bfabbfedd6
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-03 14:42:12 +0100

    {*} add button in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react"
+import Button from '@material-ui/core/Button';
 
 export default function Home() {
   return <div>
     <p>
       Clicked: <span id="value">0</span> times
-      <button id="increment">+</button>
-      <button id="decrement">-</button>
+      <Button variant="contained" color="primary">
+        +
+      </Button>
+      <Button variant="contained" color="primary">
+        -
+      </Button>
     </p>
   </div>
 }
