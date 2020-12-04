411663e74eaa25daac9242bfde709906790db26a
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-04 11:55:51 +0100

    {*} add value has textfield in counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,15 @@
 import React, { useReducer, useRef, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField'
 
 const Counter =() =>{
+  const name = "hello"
+
   return (
     <div>
       <Button variant="contained">+</Button>
       <Button variant="contained">-</Button>
-      <TextField id="standard-basic" label="choose a number" />
+      <TextField value={name} id="standard-basic" label="choose a number" />
     </div>
   )
 }
