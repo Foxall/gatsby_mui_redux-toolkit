1e1548f7bb3d3027fdd11edb3b801151bd39b1d4
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-04 16:56:07 +0100

    {*} add usedispatch in components/counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,15 +1,15 @@
 import React, { useReducer, useRef, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Counter =() =>{
 
+  const dispatch = useDispatch()
   const name = (useSelector((state) => state.counter.value))
-
   return (
     <div>
-      <Button variant="contained">+</Button>
+      <Button variant="contained" onClick={() => dispatch({ type : 'increment' })}>+</Button>
       <Button variant="contained">-</Button>
       <TextField value={name} id="standard-basic" label="choose a number" />
     </div>
