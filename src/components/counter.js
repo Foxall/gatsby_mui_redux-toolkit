2589505a689bf3289a777a541573b4643a44d92c
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-04 19:19:08 +0100

    {*} add dispatch src/components/counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -6,10 +6,10 @@ import { useSelector, useDispatch } from 'react-redux'
 const Counter =() =>{
 
   const dispatch = useDispatch()
-  const name = (useSelector((state) => state.counter.value))
+  const name = useSelector((state) => state.counter.value)
   return (
     <div>
-      <Button variant="contained" onClick={() => dispatch({ type : 'increment' })}>+</Button>
+      <Button variant="contained" onClick={() => dispatch(increment())}>+</Button>
       <Button variant="contained">-</Button>
       <TextField value={name} id="standard-basic" label="choose a number" />
     </div>
