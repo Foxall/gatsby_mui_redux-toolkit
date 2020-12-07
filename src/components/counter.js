882234450c8c26ade48715e8a0105660cb0e5fd1
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-07 09:47:10 +0100

    {*} use fct decrement in components/counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,7 +2,7 @@ import React, { useReducer, useRef, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment } from '../state/demoSlice'
+import { increment , decrement, incrementByAmount } from '../state/demoSlice'
 const Counter =() =>{
 
   const dispatch = useDispatch()
@@ -10,7 +10,7 @@ const Counter =() =>{
   return (
     <div>
       <Button variant="contained" onClick={() => dispatch(increment())}>+</Button>
-      <Button variant="contained">-</Button>
+      <Button variant="contained" onClick={() => dispatch(decrement())}>-</Button>
       <TextField value={name} id="standard-basic" label="choose a number" />
     </div>
   )
