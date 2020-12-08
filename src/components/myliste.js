d4fe5e700def39758a89f487e7bd2f31a3cc69d4
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-08 13:33:43 +0100

    {*} modifie map

--- src/components/myliste.js
+++ src/components/myliste.js
@@ -10,38 +10,18 @@ const createUsers = (numUsers = 5) => Array.from({ length: numUsers }, createUse
 
 const Users = createUsers()
 
-const name = Users.map((user, number) => <li key={number}>{user.name}</li>)
-
-const email = Users.map((user, number) => <li key={number}>{user.email}</li>)
-
-const address = Users.map((user, number) => <li key={number}>{user.address}</li>)
+const name = Users.map((user, number) => (
+  <ListItem>
+    <ListItemText key={number}>
+      {user.name}
+    </ListItemText>
+  </ListItem>
+))
 
 const Myliste = () => {
   return (
     <div>
-      <List component='item'>
-        <ListItem>
-          <ListItemText primary='item1' />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary='item2' />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary='item3' />
-        </ListItem>
-      </List>
-
-      <List component='nav' aria-label='secondary mailbox folders'>
-        <ListItem>
-          <ListItemText primary={name} />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary={email} />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary={address} />
-        </ListItem>
-      </List>
+      <List>{name}</List>
     </div>
   )
 }
