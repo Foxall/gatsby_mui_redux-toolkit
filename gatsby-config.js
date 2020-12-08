bbac0ccf7e604e4d1de2b830457c860e162bd770
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-08 09:44:07 +0100

    {*} rm option in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,6 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-material-ui`,
-      options: {
-        serialize: {
-          space: 0,
-          isJSON: true,
-          unsafe: false,
-          ignoreFunction: true
-        },
-        windowKey: '__PRELOADED_STATE__'
-      }
     }
   ]
 }
