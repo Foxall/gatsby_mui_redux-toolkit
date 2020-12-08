a57e7dca4b08ccf3389db347367a5ebd9cd82f72
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-08 16:25:57 +0100

    {*} add myliste in pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Counter from '../components/counter'
+import Myliste from '../components/myliste'
 
 export default function Home () {
   return (
     <div>
-      <Counter />
+      <Counter/>
+      <Myliste/>
     </div>
   )
 }
