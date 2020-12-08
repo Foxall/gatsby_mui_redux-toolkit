d5c98f5151daf49c8c6bc598f55c9437cbb6c142
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-08 10:53:48 +0100

    {*} add fct map in myliste

--- src/components/myliste.js
+++ src/components/myliste.js
@@ -2,14 +2,32 @@ import React from 'react'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import faker from 'faker'
+
+const createUser = () => (
+  { name: faker.name.findName(),
+  email: faker.internet.email(),
+  address: faker.address.streetAddress(),
+  bio: faker.lorem.sentence(),
+  image: faker.image.avatar(),
+  }
+)
+
+const createUsers = (numUsers = 5) => Array.from({length: numUsers}, createUser);
+
+const maps = createUsers.map.call((createUsers,i)=><li key={i} >item.name</li>)
 
 const Myliste = () => {
+
+
+
   return (
     <List component='nav' aria-label='secondary mailbox folders'>
       <ListItem button>
         <ListItemText primary='Trash' />
       </ListItem>
-    </List>
+      
+   </List>
   )
 }
 export default Myliste
