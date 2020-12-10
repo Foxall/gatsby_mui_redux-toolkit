6231b50352184a44190a7e79b82a8c6866aa8c1f
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-10 12:05:16 +0100

    {*} bad name file

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -3,7 +3,7 @@ import React from "react"
 import { Provider } from "react-redux"
 
 import configureStore from "./src/state/configureStore"
-import FakeList from "./src/state/fakeliste"
+import FakeList from "./src/state/fakelist"
 
 // eslint-disable-next-line react/display-name,react/prop-types
 export default ({ element }) => {
