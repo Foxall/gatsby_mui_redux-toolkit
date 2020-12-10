b3eae280209629d3b10c968bf0f1c2cc774ebef1
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-10 15:14:40 +0100

    {*} add button in home and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 export default function About () {
   return (
     <div>
         <h1>hello world</h1>
+        <Button variant='contained' href="/"> home </Button>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Counter from '../components/counter'
 import Myliste from '../components/myliste'
+import Button from '@material-ui/core/Button'
 
 export default function Home () {
   return (
     <div>
+       <Button variant='contained' href="/about"> about </Button>
       <Counter/>
       <Myliste/>
     </div>
