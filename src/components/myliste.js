56a209ac1f38a815968ac09e6d5b2d65fb516c5f
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-10 10:23:51 +0100

    {*} move createUser and map fct in fct Myliste

--- src/components/myliste.js
+++ src/components/myliste.js
@@ -4,19 +4,18 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import faker from 'faker'
 
-const createUser = () => ({ name: faker.name.findName(), email: faker.internet.email(), address: faker.address.streetAddress(), bio: faker.lorem.sentence(), image: faker.image.avatar(), uuid: faker.random.uuid })
-
-const createUsers = (numUsers = 5) => Array.from({ length: numUsers }, createUser)
-
-const Users = createUsers()
-
-const name = Users.map((user) => (
-  <ListItem>
-    <ListItemText key={user.uuid}>{user.name}</ListItemText>
-  </ListItem>
-))
-
 const Myliste = () => {
+
+  const createUser = () => ({ name: faker.name.findName(), email: faker.internet.email(), address: faker.address.streetAddress(), bio: faker.lorem.sentence(), image: faker.image.avatar(), uuid: faker.random.uuid })
+  const createUsers = (numUsers = 5) => Array.from({ length: numUsers }, createUser)
+  const Users = createUsers()
+
+  const name = Users.map((user) => (
+    <ListItem>
+      <ListItemText key={user.uuid}>{user.name}</ListItemText>
+    </ListItem>
+  ))
+
   return (
     <div>
       <List>{name}</List>
