4c4dfa18d2276017280d6501219099b34ddb4943
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-10 12:03:39 +0100

    {+} src/state/fakelist.js

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -3,9 +3,13 @@ import React from "react"
 import { Provider } from "react-redux"
 
 import configureStore from "./src/state/configureStore"
+import FakeList from "./src/state/fakeliste"
 
 // eslint-disable-next-line react/display-name,react/prop-types
 export default ({ element }) => {
   const store = configureStore()
-  return <Provider store={store}>{element}</Provider>
+  return <Provider store={store}>
+    <FakeList/>
+    {element}
+  </Provider>
 }
