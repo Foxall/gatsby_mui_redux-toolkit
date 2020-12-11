0d66d35f3b84e9aa2712fa85244fdd4941c7d82d
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-11 13:50:52 +0100

    {*} move uuid in the top of the list

--- src/state/fakelist.js
+++ src/state/fakelist.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 
 const SetFakeList = () => {
   const dispatch = useDispatch()
-  const createUser = () => ({ name: faker.name.findName(), email: faker.internet.email(), address: faker.address.streetAddress(), bio: faker.lorem.sentence(), image: faker.image.avatar(), uuid: faker.random.uuid() })
+  const createUser = () => ({ uuid: faker.random.uuid(), email: faker.internet.email(), address: faker.address.streetAddress(), bio: faker.lorem.sentence(), image: faker.image.avatar(), name: faker.name.findName() })
   const createUsers = (numUsers = 5) => Array.from({ length: numUsers }, createUser)
   const Users = createUsers()
   dispatch(setRedux(Users))
